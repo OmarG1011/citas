78842723ca7e394739cd0f5a1da3a9fead3db5f2
omar <omartega2512@gmail.com>
2025-02-21 16:11:58 -0600

    cambio bd

--- Model/BD.php
+++ Model/BD.php
@@ -1,20 +1,20 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "CitasMedicas";
-    private $user = "omar";
-    private $pass = "12345678";
-    private $port = "1433";
-    public $conn;
+    private $host = "localhost"; // Dirección del servidor
+    private $db_name = "CitasMedicas"; // Nombre de la base de datos
+    private $user = "root"; // Usuario de MySQL
+    private $pass = ""; // Contraseña de MySQL
+    private $conn;
 
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("sqlsrv:server=$this->host,$this->port;Database=$this->db_name", $this->user, $this->pass);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // Cambiar la cadena de conexión para MySQL
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Establecer el modo de error
         } catch (PDOException $e) {
-            die("Error de conexión: " . $e->getMessage());
+            die("Error de conexión: " . $e->getMessage()); // En caso de error, mostrar mensaje
         }
 
         return $this->conn;
